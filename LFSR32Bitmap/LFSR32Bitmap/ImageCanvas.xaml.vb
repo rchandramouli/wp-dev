d11010385953382bd7648ade60dcfc718ce5d1e1
Ramasamy Chandramouli <rchandramouli@live.com>
2015-06-30 18:45:40 +0530

    LFSR32Bitmap initial commit
    LFSR degree-32 polynomial bitmap painter

---
+++ LFSR32Bitmap/LFSR32Bitmap/ImageCanvas.xaml.vb
@@ -0,0 +1,195 @@
+﻿Imports System.Windows.Media.Imaging
+
+Partial Public Class Page1
+    Inherits PhoneApplicationPage
+
+    Private seed_red As UInteger
+    Private seed_blue As UInteger
+    Private seed_green As UInteger
+    Private paint_steps As Integer
+    Private poly_array(32) As UInteger
+
+    Private Function L_SHFT32(val As UInteger, shft As UInteger) As UInteger
+        Dim ex_val As UInt64
+
+        ex_val = val
+        ex_val = ex_val * (2 ^ shft)
+        ex_val = ex_val And &HFFFFFFFF
+
+        Return ex_val
+    End Function
+
+    Private Function L_SHFT16(val As UInteger, shft As UInteger) As UInteger
+        val = val And &HFFFF
+        Return ((val * (2 ^ shft)) And &HFFFF)
+    End Function
+
+    Private Function L_SHFT8(val As UInteger, shft As UInteger) As UInteger
+        val = val And &HFF
+        Return ((val * (2 ^ shft)) And &HFF)
+    End Function
+
+    Private Function R_SHFT32(val As UInteger, shft As UInteger) As UInteger
+        Return ((val And &HFFFFFFFF) / (2 ^ shft))
+    End Function
+
+    Private Function R_SHFT16(val As UInteger, shft As UInteger) As UInteger
+        Return ((val And &HFFFF) / (2 ^ shft))
+    End Function
+
+    Private Function R_SHFT8(val As UInteger, shft As UInteger) As UInteger
+        Return ((val And &HFF) / (2 ^ shft))
+    End Function
+
+    Private Function LFSR_N(lf_val As UInteger, N As UInteger) As UInteger
+        Dim bit As UInteger, mask As UInteger
+        Dim idx As UInteger
+
+        mask = (2 ^ (N - 1)) - 1
+        mask = 2 * mask + 1
+
+        lf_val = lf_val And mask
+        bit = 0
+        For idx = 0 To poly_array.Length - 1
+            If poly_array(idx) = 0 Then
+                Exit For
+            End If
+            bit = R_SHFT32(lf_val, N - poly_array(idx)) Xor bit
+        Next
+        bit = bit And &H1
+        lf_val = R_SHFT32(lf_val, 1) Or L_SHFT32(bit, N - 1)
+
+        Return lf_val
+    End Function
+
+    Private Function LFSR32(lf_val As UInteger) As UInteger
+        Return LFSR_N(lf_val, 32)
+    End Function
+
+    Private Function LFSR24(lf_val As UInteger) As UInteger
+        Return LFSR_N(lf_val, 24)
+    End Function
+
+    Private Function LFSR8(lf_val As UInteger) As UInteger
+        Return LFSR_N(lf_val, 8)
+    End Function
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        Dim poly_string As String
+        Dim str_array() As String
+        Dim idx As Integer
+
+        MyBase.OnNavigatedTo(e)
+
+        NavigationContext.QueryString.TryGetValue("red", seed_red)
+        NavigationContext.QueryString.TryGetValue("green", seed_green)
+        NavigationContext.QueryString.TryGetValue("blue", seed_blue)
+        NavigationContext.QueryString.TryGetValue("steps", paint_steps)
+
+        poly_string = ""
+        NavigationContext.QueryString.TryGetValue("poly", poly_string)
+
+        str_array = poly_string.Split(",")
+        For idx = 0 To 31
+            poly_array(idx) = 0
+        Next
+        For idx = 1 To str_array.Length - 1
+            poly_array(idx - 1) = Convert.ToInt32(str_array(idx))
+        Next
+    End Sub
+
+    Private Sub Page1_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim bmp As New WriteableBitmap(canvas.Width, canvas.Height)
+        Dim w As UInteger, h As UInteger
+        Dim x As UInteger, y As UInteger
+        Dim dxstep As Integer, dystep As Integer
+        Dim x_symm As Integer, y_symm As Integer
+
+        Dim lfr_val As UInteger, lfg_val As UInteger, lfb_val As UInteger
+        Dim clr As Integer
+
+        w = bmp.PixelWidth
+        h = bmp.PixelHeight
+
+        lfr_val = seed_red
+        lfg_val = seed_green
+        lfb_val = seed_blue
+
+        ' No symmetry
+        x_symm = 0
+        y_symm = 0
+
+        For x = 0 To (w / (1 + x_symm) - paint_steps) Step paint_steps
+            For y = 0 To (h / (1 + y_symm) - paint_steps) Step paint_steps
+                ' Form-1
+                'clr = lfr_val And &HFF
+                'clr = L_SHFT32((lfg_val And &HFF), 8) Or clr
+                'clr = L_SHFT32((lfb_val And &HFF), 16) Or clr
+                'clr = clr Or &HFF000000
+
+                ' Form-2
+                'clr = (lfr_val Xor lfg_val Xor lfb_val) And &H7FFFFFFF
+                'clr = clr Or &H80000000
+
+                ' Form-3
+                clr = (lfr_val And &HFF)
+                clr = clr Or (lfg_val And &HFF00)
+                clr = clr Or (lfb_val And &HFF0000)
+                clr = clr Or ((lfr_val Xor lfg_val Xor lfb_val) And &H7F000000)
+                clr = clr Or &H80000000
+
+                ' Form-4
+                ' clr = (lfr_val And &H7FFFFFFFUI)
+                ' If (lfr_val And &H80000000) <> 0 Then
+                ' clr = clr Or &H80000000
+                ' End If
+
+                ' 4-Quad N-step symmetric paint
+                If x_symm <> 0 And y_symm <> 0 Then
+                    For dxstep = 0 To paint_steps - 1
+                        For dystep = 0 To paint_steps - 1
+                            bmp.Pixels((x + dxstep) + (y + dystep) * w) = clr
+                            bmp.Pixels((w - 1 - (x + dxstep)) + (y + dystep) * w) = clr
+                            bmp.Pixels((x + dxstep) + (h - 1 - (y + dystep)) * w) = clr
+                            bmp.Pixels((w - 1 - (x + dxstep)) + (h - 1 - (y + dystep)) * w) = clr
+                        Next
+                    Next
+                ElseIf x_symm <> 0 Then
+                    For dxstep = 0 To paint_steps - 1
+                        For dystep = 0 To paint_steps - 1
+                            bmp.Pixels((x + dxstep) + (y + dystep) * w) = clr
+                            bmp.Pixels((w - 1 - (x + dxstep)) + (y + dystep) * w) = clr
+                        Next
+                    Next
+                ElseIf y_symm <> 0 Then
+                    For dxstep = 0 To paint_steps - 1
+                        For dystep = 0 To paint_steps - 1
+                            bmp.Pixels((x + dxstep) + (y + dystep) * w) = clr
+                            bmp.Pixels((x + dxstep) + (h - 1 - (y + dystep)) * w) = clr
+                        Next
+                    Next
+                Else
+                    For dxstep = 0 To paint_steps - 1
+                        For dystep = 0 To paint_steps - 1
+                            bmp.Pixels((x + dxstep) + (y + dystep) * w) = clr
+                        Next
+                    Next
+                End If
+
+                lfr_val = LFSR32(lfr_val)
+                lfg_val = LFSR32(lfg_val)
+                lfb_val = LFSR32(lfb_val)
+            Next
+        Next
+        bmp.Invalidate()
+        canvas.Source = bmp
+    End Sub
+
+    Private Sub canvas_DoubleTap(sender As Object, e As Input.GestureEventArgs) Handles canvas.DoubleTap
+        NavigationService.GoBack()
+    End Sub
+End Class
---
+++ LFSR32Bitmap/LFSR32Bitmap/MainPage.xaml.vb
@@ -0,0 +1,244 @@
+﻿Imports System
+Imports System.Threading
+Imports System.Windows.Controls
+Imports System.Windows.Media.Imaging
+Imports Microsoft.Phone.Controls
+Imports Microsoft.Phone.Shell
+Imports Microsoft.Phone.PictureDecoder
+
+Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+    Private commonSliderMode As Boolean
+
+    Private Sub FlipBitTag(rbit As Rectangle)
+        If Convert.ToInt32(rbit.Tag) = 0 Then
+            rbit.Fill = New SolidColorBrush(Color.FromArgb(&HFF, &HFF, &HFF, &HFF))
+            rbit.Tag = 1
+        Else
+            rbit.Fill = New SolidColorBrush(Color.FromArgb(&HFF, 0, 0, 0))
+            rbit.Tag = 0
+        End If
+    End Sub
+
+    ' Constructor
+    Public Sub New()
+        Dim rct As Rectangle
+        Dim idx As Integer
+
+        InitializeComponent()
+
+        commonSliderMode = False
+
+        ' Load with default RGB values in the slider
+        lblRed.Text = Convert.ToString(Convert.ToInt32(sldRed.Value), 16)
+        lblBlue.Text = Convert.ToString(Convert.ToInt32(sldBlue.Value), 16)
+        lblGreen.Text = Convert.ToString(Convert.ToInt32(sldGreen.Value), 16)
+        lblPaintSteps.Text = Convert.ToString(Convert.ToInt32(sldPaintSteps.Value))
+
+        ' Load default polynomial
+        ' LFSR32: x^32 + x^22 + x^2 + x + 1
+        For idx = 1 To 32
+            rct = FindName("rbit" & Convert.ToString(idx))
+            If idx = 1 Or idx = 2 Or idx = 22 Or idx = 32 Then
+                rct.Tag = 0
+            Else
+                rct.Tag = 1
+            End If
+            FlipBitTag(rct)
+        Next
+
+        SupportedOrientations = SupportedPageOrientation.Portrait Or SupportedPageOrientation.Landscape
+
+        ' Sample code to localize the ApplicationBar
+        'BuildLocalizedApplicationBar()
+
+    End Sub
+
+    ' Sample code for building a localized ApplicationBar
+    'Private Sub BuildLocalizedApplicationBar()
+    '    ' Set the page's ApplicationBar to a new instance of ApplicationBar.
+    '    ApplicationBar = New ApplicationBar()
+
+    '    ' Create a new button and set the text value to the localized string from AppResources.
+    '    Dim appBarButton As New ApplicationBarIconButton(New Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative))
+    '    appBarButton.Text = AppResources.AppBarButtonText
+    '    ApplicationBar.Buttons.Add(appBarButton)
+
+    '    ' Create a new menu item with the localized string from AppResources.
+    '    Dim appBarMenuItem As New ApplicationBarMenuItem(AppResources.AppBarMenuItemText)
+    '    ApplicationBar.MenuItems.Add(appBarMenuItem)
+    'End Sub
+
+    Private Sub btnPaint_Click(sender As Object, e As RoutedEventArgs) Handles btnPaint.Click
+        Dim uriParams As String
+        Dim rct As Rectangle
+        Dim idx As Integer
+
+        If commonSliderMode = True Then
+            uriParams = "red=" & Convert.ToInt32(sldRed.Value) & "&" & _
+                       "green=" & Convert.ToInt32(sldRed.Value) & "&" & _
+                       "blue=" & Convert.ToInt32(sldRed.Value) & "&" & _
+                       "steps=" & Convert.ToInt32(sldPaintSteps.Value)
+        Else
+            uriParams = "red=" & Convert.ToInt32(sldRed.Value) & "&" & _
+                       "green=" & Convert.ToInt32(sldGreen.Value) & "&" & _
+                       "blue=" & Convert.ToInt32(sldBlue.Value) & "&" & _
+                       "steps=" & Convert.ToInt32(sldPaintSteps.Value)
+        End If
+        uriParams = uriParams & "&" & "poly=0"
+        For idx = 1 To 32
+            rct = FindName("rbit" & idx)
+            If Convert.ToInt32(rct.Tag) = 1 Then
+                uriParams = uriParams & "," & Convert.ToString(idx)
+            End If
+        Next
+
+        NavigationService.Navigate(New Uri("/ImageCanvas.xaml?" & uriParams, UriKind.Relative))
+    End Sub
+
+    Private Sub ToggleSliderMode()
+        If commonSliderMode = True Then
+            commonSliderMode = False
+            sldBlue.IsEnabled = True
+            sldGreen.IsEnabled = True
+        Else
+            commonSliderMode = True
+            sldBlue.IsEnabled = False
+            sldGreen.IsEnabled = False
+        End If
+    End Sub
+
+    Private Sub sldBlue_Hold(sender As Object, e As Input.GestureEventArgs) Handles sldBlue.Hold
+        ToggleSliderMode()
+    End Sub
+
+    Private Sub sldBlue_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double)) Handles sldBlue.ValueChanged
+        If Convert.ToInt32(e.NewValue) <> Convert.ToInt32(e.OldValue) Then
+            lblBlue.Text = Convert.ToString(Convert.ToInt32(e.NewValue), 16)
+        End If
+    End Sub
+
+    Private Sub sldGreen_Hold(sender As Object, e As Input.GestureEventArgs) Handles sldGreen.Hold
+        ToggleSliderMode()
+    End Sub
+
+    Private Sub sldGreen_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double)) Handles sldGreen.ValueChanged
+        If Convert.ToInt32(e.NewValue) <> Convert.ToInt32(e.OldValue) Then
+            lblGreen.Text = Convert.ToString(Convert.ToInt32(e.NewValue), 16)
+        End If
+    End Sub
+
+    Private Sub sldRed_Hold(sender As Object, e As Input.GestureEventArgs) Handles sldRed.Hold
+        ToggleSliderMode()
+    End Sub
+
+    Private Sub sldRed_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double)) Handles sldRed.ValueChanged
+        If Convert.ToInt32(e.NewValue) <> Convert.ToInt32(e.OldValue) Then
+            lblRed.Text = Convert.ToString(Convert.ToInt32(e.NewValue), 16)
+        End If
+    End Sub
+
+    Private Sub sldPaintSteps_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double)) Handles sldPaintSteps.ValueChanged
+        If Convert.ToInt32(e.NewValue) <> Convert.ToInt32(e.OldValue) Then
+            lblPaintSteps.Text = Convert.ToString(Convert.ToInt32(e.NewValue))
+        End If
+    End Sub
+
+    Private Sub rbit1_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit1.Tap
+        FlipBitTag(rbit1)
+    End Sub
+    Private Sub rbit2_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit2.Tap
+        FlipBitTag(rbit2)
+    End Sub
+    Private Sub rbit3_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit3.Tap
+        FlipBitTag(rbit3)
+    End Sub
+    Private Sub rbit4_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit4.Tap
+        FlipBitTag(rbit4)
+    End Sub
+    Private Sub rbit5_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit5.Tap
+        FlipBitTag(rbit5)
+    End Sub
+    Private Sub rbit6_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit6.Tap
+        FlipBitTag(rbit6)
+    End Sub
+    Private Sub rbit7_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit7.Tap
+        FlipBitTag(rbit7)
+    End Sub
+    Private Sub rbit8_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit8.Tap
+        FlipBitTag(rbit8)
+    End Sub
+    Private Sub rbit9_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit9.Tap
+        FlipBitTag(rbit9)
+    End Sub
+    Private Sub rbit10_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit10.Tap
+        FlipBitTag(rbit10)
+    End Sub
+    Private Sub rbit11_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit11.Tap
+        FlipBitTag(rbit11)
+    End Sub
+    Private Sub rbit12_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit12.Tap
+        FlipBitTag(rbit12)
+    End Sub
+    Private Sub rbit13_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit13.Tap
+        FlipBitTag(rbit13)
+    End Sub
+    Private Sub rbit14_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit14.Tap
+        FlipBitTag(rbit14)
+    End Sub
+    Private Sub rbit15_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit15.Tap
+        FlipBitTag(rbit15)
+    End Sub
+    Private Sub rbit16_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit16.Tap
+        FlipBitTag(rbit16)
+    End Sub
+    Private Sub rbit17_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit17.Tap
+        FlipBitTag(rbit17)
+    End Sub
+    Private Sub rbit18_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit18.Tap
+        FlipBitTag(rbit18)
+    End Sub
+    Private Sub rbit19_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit19.Tap
+        FlipBitTag(rbit19)
+    End Sub
+    Private Sub rbit20_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit20.Tap
+        FlipBitTag(rbit20)
+    End Sub
+    Private Sub rbit21_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit21.Tap
+        FlipBitTag(rbit21)
+    End Sub
+    Private Sub rbit22_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit22.Tap
+        FlipBitTag(rbit22)
+    End Sub
+    Private Sub rbit23_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit23.Tap
+        FlipBitTag(rbit23)
+    End Sub
+    Private Sub rbit24_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit24.Tap
+        FlipBitTag(rbit24)
+    End Sub
+    Private Sub rbit25_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit25.Tap
+        FlipBitTag(rbit25)
+    End Sub
+    Private Sub rbit26_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit26.Tap
+        FlipBitTag(rbit26)
+    End Sub
+    Private Sub rbit27_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit27.Tap
+        FlipBitTag(rbit27)
+    End Sub
+    Private Sub rbit28_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit28.Tap
+        FlipBitTag(rbit28)
+    End Sub
+    Private Sub rbit29_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit29.Tap
+        FlipBitTag(rbit29)
+    End Sub
+    Private Sub rbit30_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit30.Tap
+        FlipBitTag(rbit30)
+    End Sub
+    Private Sub rbit31_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit31.Tap
+        FlipBitTag(rbit31)
+    End Sub
+    Private Sub rbit32_Tap(sender As Object, e As Input.GestureEventArgs) Handles rbit32.Tap
+        FlipBitTag(rbit32)
+    End Sub
+End Class
